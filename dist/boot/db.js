eef0e1152a1c9ec71f4ba2d4c321c2fd34695101
horasikus <hglagan@gmail.com>
2018-11-07 18:09:53 +0100

    id parkingMeter

--- dist/boot/db.js
+++ dist/boot/db.js
@@ -1,38 +1,39 @@
 /*
 import { join } from 'path';
-import parse from 'csv-parse';
+import moment from 'moment';
+import csv from 'csv-parse';
 import { createReadStream, readdirSync } from 'fs';
 
 module.exports = function (app) {
   readdirSync('db').forEach(fileName => {
     const file = join('db', fileName);
+    const results = [];
     createReadStream(file)
-      .pipe(parse({ delimiter: ';' }))
+      .pipe(csv({ delimiter: ';', skip_empty_lines: true, skip_lines_with_empty_values: true }))
       .on('data', csvrow => {
-        app.models.Transaction.create({
-          id: csvrow[9],
-          parkingMeter_id: csvrow[3],
-          date: csvrow[1],
-          bookingStartDate: csvrow[2],
-          bookingEndDate: csvrow[23],
-          paidMinutes: csvrow[8],
-          freeMinutes: csvrow[25],
-          rate: csvrow[15],
-          customerType: csvrow[22],
-          ticketNumber: csvrow[10],
-          price: csvrow[4],
-          paymentID: csvrow[9],
-          paymentType: csvrow[21],
-          paymentMethod: csvrow[0],
-          totalMinutes: csvrow[7],
-        }, (err, transaction) => {
-          if (err) {
-            if (err) throw err;
-            console.log(transaction);
-          }
+        results.push({
+          id: csvrow[9] || 0,
+          parkingMeter_id: csvrow[3] || 0,
+          date: moment(csvrow[1], 'DD/MM/YYYY HH:mm').toDate(),
+          bookingStartDate: moment(csvrow[2], 'DD/MM/YYYY HH:mm').toDate(),
+          bookingEndDate: moment(csvrow[23], 'DD/MM/YYYY HH:mm').toDate(),
+          paidMinutes: csvrow[8] || 0,
+          freeMinutes: csvrow[25] || 0,
+          rate: 'other',
+          customerType: csvrow[22] || '',
+          ticketNumber: csvrow[10] || 0,
+          price: csvrow[4].replace(',', '.') || 0,
+          paymentID: csvrow[9] || 0,
+          paymentType: 'parkingMeter',
+          paymentMethod: csvrow[0] || 'other',
+          totalMinutes: csvrow[7] || 0,
         });
       })
       .on('end', () => {
+        app.models.Transaction.create(results, (err, instance, created) => {
+          if (err) console.log(instance);
+        });
+        console.log('finished: ', file);
       });
   });
 };
